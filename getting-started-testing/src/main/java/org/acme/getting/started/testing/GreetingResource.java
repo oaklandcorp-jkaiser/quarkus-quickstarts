84859a584ff0ddd33deefc1d0f5dfcde89032a80
Jason Kaiser <jkaiser@oaklandcorp.com>
2026-03-20 14:25:30 -0500

    Added jaxb to the getting-started-testing project to show case a bug.

--- getting-started-testing/src/main/java/org/acme/getting/started/testing/GreetingResource.java
+++ getting-started-testing/src/main/java/org/acme/getting/started/testing/GreetingResource.java
@@ -1,10 +1,18 @@
 package org.acme.getting.started.testing;
 
+import java.io.ByteArrayInputStream;
+
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 @Path("/hello")
 public class GreetingResource {
@@ -21,7 +29,28 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String hello() throws JAXBException {
+        var record = "<greeting message=\"hello\"></greeting>";
+        var unmarshaller = JAXBContext.newInstance(GreetingRecord.class).createUnmarshaller();
+        var greeting = (GreetingRecord) unmarshaller.unmarshal(new ByteArrayInputStream(record.getBytes()));
+        return greeting.getMessage();
     }
+
+    @XmlRootElement(name = "greeting")
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class GreetingRecord {
+
+        @XmlAttribute(name = "message", required = true)
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+    }
+
 }
